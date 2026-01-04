87833edffa94064586b242965822e33d01831e17
IT Development <it.dev@mjm-bali.co.id>
2026-01-05 01:44:41 +0800

    sudah hampir jadi

--- app/Http/Controllers/dashboardController.php
+++ app/Http/Controllers/dashboardController.php
@@ -313,7 +313,7 @@ public function update(Request $request, string $hash)
         $userName = $ticket->user->employee->employee_name;
 
         $message =
-            "*Ticket Updated*\n" .
+            "*IT Ticket Updated*\n" .
             "Queue: {$ticket->queue_number}\n" .
             "Date: {$formattedDate}\n" .
             "User: {$userName}\n" .
@@ -322,44 +322,38 @@ public function update(Request $request, string $hash)
             "Status: {$ticket->status}\n" .
             "Priority: {$ticket->priority}\n" .
             "Executor: {$executorName}\n";
-            // if ($ticket->status === 'Progress' && $ticket->estimation) {
-            //   $estimationFormatted = $ticket->estimation
-            //   ->timezone('Asia/Makassar')
-            //   ->format('d-m-Y H:i');
-            //   $message .= "Estimation: {$estimationFormatted}\n";
-            // }
-            // if ($ticket->status === 'Closed' && $ticket->estimation && $ticket->finished) {
-            //   $estimationFormatted = $ticket->estimation
-            //   ->timezone('Asia/Makassar')
-            //   ->format('d-m-Y H:i');
-            //   $finishedFormatted = $ticket->finished
-            //   ->timezone('Asia/Makassar')
-            //   ->format('d-m-Y H:i');
-            //   $message .= "Estimation: {$estimationFormatted}\n";
-            //   $message .= "Finished: {$finishedFormatted}\n";
-            // }
+            if ($ticket->status === 'Progress' && $ticket->estimation) {
+              $estimationFormatted = $ticket->estimation
+              ->timezone('Asia/Makassar')
+              ->format('d-m-Y H:i');
+              $message .= "Estimation: {$estimationFormatted}\n";
+            }
+            if ($ticket->status === 'Closed' && $ticket->estimation && $ticket->finished) {
+              $estimationFormatted = $ticket->estimation
+              ->timezone('Asia/Makassar')
+              ->format('d-m-Y H:i');
+              $finishedFormatted = $ticket->finished
+              ->timezone('Asia/Makassar')
+              ->format('d-m-Y H:i');
+              $message .= "Estimation: {$estimationFormatted}\n";
+              $message .= "Finished: {$finishedFormatted}\n";
+            }
             // Closed → tampilkan estimation & finished
-if ($ticket->status === 'Closed') {
-
-    if ($ticket->estimation) {
-        $message .= "Estimation: " .
-            $ticket->estimation
-                ->timezone('Asia/Makassar')
-                ->format('d-m-Y H:i') . "\n";
-    }
-
-    if ($ticket->finished) {
-        $message .= "Finished: " .
-            $ticket->finished
-                ->timezone('Asia/Makassar')
-                ->format('d-m-Y H:i') . "\n";
-    }
-}
-
-            
+// if ($ticket->status === 'Closed') {
+//     if ($ticket->estimation) {
+//         $message .= "Estimation: " .
+//             $ticket->estimation
+//                 ->timezone('Asia/Makassar')
+//                 ->format('d-m-Y H:i') . "\n";
+//     }
+//     if ($ticket->finished) {
+//         $message .= "Finished: " .
+//             $ticket->finished
+//                 ->timezone('Asia/Makassar')
+//                 ->format('d-m-Y H:i') . "\n";
+//     }
+// }           
            $message .= "\nAdmin Link:\n{$adminUrl}";
-
-
         Http::timeout(15)->post('http://127.0.0.1:3000/send-message', [
             'group_id' => '120363405189832865@g.us',
             'text'     => $message,
